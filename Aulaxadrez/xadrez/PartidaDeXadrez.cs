ddc80e309c9bc103cc2666fa7b3e048225c809d0
Thauane Silva <Thauane Silva>
2016-10-30 10:08:49 -0200

    Implementando XequeMate

--- Aulaxadrez/xadrez/PartidaDeXadrez.cs
+++ Aulaxadrez/xadrez/PartidaDeXadrez.cs
@@ -157,12 +157,13 @@ namespace xadrez {
                 for (int i = 0; i < tab.linhas; i++) {
                     for (int j = 0; j < tab.colunas; j++) {
                         if (mat[i, j]) {
+                            Posicao origem = x.posicao;
                             Posicao destino = new Posicao(i, j);
-                            Peca pecaCapturada = executaMovimento(x.posicao, new Posicao(i, j));
+                            Peca pecaCapturada = executaMovimento(origem, destino);
                             bool testeXeque = estaEmXeque(cor);
-                            desfazMovimento(x.posicao, destino, pecaCapturada);
+                            desfazMovimento(origem, destino, pecaCapturada);
                             if (!testeXeque) {
-                                return false;
+                                return false; 
                             }
                         }
                     }
